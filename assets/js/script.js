acbe4b7ba39fcc5dae331d27bc7c1d9c2d4aa85a
jorge2101719 <rusosky01@gmail.com>
2023-06-03 17:47:24 -0400

    construida la primera parte de la función que se activa con evento Click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let cantidad_a_comprar = document.querySelector('#cantidad_a_comprar');
+let cantidad_incorrecta = document.querySelector('#cantidad_incorrecta');
 let color_seleccionado = document.querySelector('#color_seleccionado');
 let calcular = document.querySelector('#calcular');
 let total_pagar = document.querySelector('#total_pagar');
@@ -8,5 +9,13 @@ let mi_color = document.querySelector('#mi_color');
 calcular.addEventListener('click', miFuncion);
 
 function miFuncion () {
-    
+    console.log('dentro de la función');
+    if (cantidad_a_comprar.value < 0) {
+        console.log(cantidad_a_comprar.value);
+        cantidad_incorrecta.innerHTML = 'Por favor, ingrese un valor positivo';
+    } else if (cantidad_a_comprar.value == 0) {
+        cantidad_incorrecta.innerHTML = 'Procure que la cantidad sea al menos 1'
+    } else {
+        
+    }
 }
