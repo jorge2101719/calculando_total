d77dc51f6269e3e51ad4c087ed0533af6cda95e2
jorge2101719 <rusosky01@gmail.com>
2023-06-03 17:16:05 -0400

    se vincula oídor al botón Calcular Total; inicio la definición de la función

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,9 @@ let calcular = document.querySelector('#calcular');
 let total_pagar = document.querySelector('#total_pagar');
 let total_articulos = document.querySelector('#total_articulos');
 let mi_color = document.querySelector('#mi_color');
+
+calcular.addEventListener('click', miFuncion);
+
+function miFuncion () {
+    
+}
