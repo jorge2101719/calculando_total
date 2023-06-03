b52305de731f2404e7cf048957efdce40c01f4fa
jorge2101719 <rusosky01@gmail.com>
2023-06-03 18:10:06 -0400

    se agrega espacio para mensajes de error de color; se completa la función de eventos para que haga los cálculos y limpie los campos

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,26 @@ let calcular = document.querySelector('#calcular');
 let total_pagar = document.querySelector('#total_pagar');
 let total_articulos = document.querySelector('#total_articulos');
 let mi_color = document.querySelector('#mi_color');
+let color_error = document.querySelector('#color_error');
 
 calcular.addEventListener('click', miFuncion);
 
 function miFuncion () {
     console.log('dentro de la función');
     if (cantidad_a_comprar.value < 0) {
-        console.log(cantidad_a_comprar.value);
         cantidad_incorrecta.innerHTML = 'Por favor, ingrese un valor positivo';
     } else if (cantidad_a_comprar.value == 0) {
         cantidad_incorrecta.innerHTML = 'Procure que la cantidad sea al menos 1'
+    } else if(color_seleccionado.value === '' ) {
+        color_error.innerHTML = 'Por favor, seleccione un color'
     } else {
-        
+        cantidad_incorrecta.innerHTML = '';
+        color_error.innerHTML = '';
+        total_pagar.innerHTML = '$ ' + 400000 * cantidad_a_comprar.value;
+        total_articulos.innerHTML = cantidad_a_comprar.value;
+        mi_color.style.backgroundColor = color_seleccionado.value;
     }
+    // instrucciones que limpian los campos Cantidad y Color, de la tarjeta
+    cantidad_a_comprar.value = '';
+    color_seleccionado.value = ''
 }
