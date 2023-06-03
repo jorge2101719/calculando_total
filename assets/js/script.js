9bfaa5c7947731398b8d770bd02892ce8b322779
jorge2101719 <rusosky01@gmail.com>
2023-06-03 17:13:48 -0400

    pequeños cambios en css, vinculación del DOM con javascript

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let cantidad_a_comprar = document.querySelector('#cantidad_a_comprar');
+let color_seleccionado = document.querySelector('#color_seleccionado');
+let calcular = document.querySelector('#calcular');
+let total_pagar = document.querySelector('#total_pagar');
+let total_articulos = document.querySelector('#total_articulos');
+let mi_color = document.querySelector('#mi_color');
